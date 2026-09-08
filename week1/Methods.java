9e8b6c997e0b359ba2bb524a451d0a8dda931787
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-08 15:49:51 +0200

    Practice method overloading

--- week1/Methods.java
+++ week1/Methods.java
@@ -3,7 +3,11 @@ public class Methods {
       public static void main(String[] args) {
         sayHello("Aubierge", 25);
         sayHello("Flora", 30);
-
+        int product = multiply(4, 5);
+        System.out.println("The product is: " + product);
+        
+        int products = multiply(2, 3, 4);
+        System.out.println("The product of three numbers is:" + products);
         int sum = add(5, 10);
         System.out.println("The sum is: " + sum);
     }
@@ -14,6 +18,13 @@ public class Methods {
 
     public static int add(int a, int b) {
     return a + b;
-}
+    }
+    public static int multiply(int a, int b) {
+    return a * b;
+    }
+
+    public static int multiply(int a, int b, int c) {
+    return a * b * c;
+    }
 
 }

3c0c08aa353de39e705e30a4339de75de3699dff
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-08 08:32:18 +0200

    variables

--- week1/Main.java
+++ week1/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args){
+        int age = 16;
+        double average = 96.5;
+        String name = "Aubierge";
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println(age);
+        System.out.println(average);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(isStudent);
+
         System.out.println("Hello, Java!");
     }
+
 }

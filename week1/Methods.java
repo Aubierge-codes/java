091bde174e9760da6e35a4fb8b24424b5f2d4c6c
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-08 11:11:04 +0200

    Practice methods with return values

--- week1/Methods.java
+++ week1/Methods.java
@@ -4,11 +4,16 @@ public class Methods {
         sayHello("Aubierge", 25);
         sayHello("Flora", 30);
 
-       
+        int sum = add(5, 10);
+        System.out.println("The sum is: " + sum);
     }
 
     public static void sayHello(String name, int age) {
     System.out.println("Hello, " + name + "!"  + "You are " + age + " years old.");
     }
+
+    public static int add(int a, int b) {
+    return a + b;
+}
 
 }

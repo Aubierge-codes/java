826d4f283fb177d35ff61242169498ef18e75dba
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-08 09:34:10 +0200

    Practice switch statements

--- week1/Main.java
+++ week1/Main.java
@@ -48,6 +48,28 @@ public class Main {
         System.out.println(age >= 18 || score >= 80);
         System.out.println(!hasPermission);
 
+        int number = 3;
+
+switch (number) {
+    case 1:
+        System.out.println("One");
+        break;
+
+    case 2:
+        System.out.println("Two");
+        break;
+
+    case 3:
+        System.out.println("Three");
+        break;
+
+    case 4:
+        System.out.println("Four");
+        break;
+
+    default:
+        System.out.println("Something else");
+}
     }
 
 }

de4c5c2edb697a49d422aefd9fe2d8b4481ef2a1
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-08 10:17:35 +0200

    Practice Java methods and parameters

---
+++ week1/Methods.java
@@ -0,0 +1,14 @@
+public class Methods {
+ 
+      public static void main(String[] args) {
+        sayHello("Aubierge", 25);
+        sayHello("Flora", 30);
+
+       
+    }
+
+    public static void sayHello(String name, int age) {
+    System.out.println("Hello, " + name + "!"  + "You are " + age + " years old.");
+    }
+
+}

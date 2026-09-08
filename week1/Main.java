ca552ba711dfe487d912fb590f9cbdbc67072230
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-08 09:51:36 +0200

    Practice nested loops

--- week1/Main.java
+++ week1/Main.java
@@ -78,9 +78,13 @@ while (number <= 3) {
     number = number + 1;
 }
 
-for (int i = 1; i <= 3; i = i + 1) {
-    System.out.println(i);
-}
+
+    for (int i = 1; i <= 2; i = i + 1) {
+
+    for (int j = 1; j <= 3; j = j + 1) {
+        System.out.println(i + " " + j);
     }
+    }
+}
 
 }

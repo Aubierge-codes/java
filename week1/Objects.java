5a0dc1efa2240881fc164ce32962600347c2ea7d
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-09 14:46:06 +0200

    Create Student class

---
+++ week1/Objects.java
@@ -0,0 +1,16 @@
+public class Objects {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Student {
+
+    String name;
+    int age;
+
+    void introduce() {
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+}

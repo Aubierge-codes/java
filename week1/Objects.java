073b33bc58e27682ac62010c98e77046cf8b9eb7
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-10 09:08:12 +0200

    Learn the this keyword

--- week1/Objects.java
+++ week1/Objects.java
@@ -2,11 +2,13 @@ public class Objects {
 
     public static void main(String[] args) {
 
-        Student student1 = new Student("Aubierge", 16);
-        Student student2 = new Student("Flora", 17);
+        Student student1 = new Student("Aubierge", 16, "High School");
+        Student student2 = new Student("Flora", 17, "High School");
+        Student student3 = new Student("Liam", 18, "University");
 
         student1.introduce();
         student2.introduce();
+        student3.introduce();
     }
 }
 
@@ -14,13 +16,19 @@ class Student {
 
     String name;
     int age;
+    String school;
 
-    public Student(String name, int age) {
+    public Student(String name, int age, String school) {
         this.name = name;
         this.age = age;
+        this.school = school;
     }
 
     void introduce() {
-        System.out.println("My name is " + name + " and I am " + age + " years old.");
+        System.out.println(
+            "My name is " + this.name +
+            ", I am " + this.age +
+            " years old, and I study at " + this.school + "."
+        );
     }
 }

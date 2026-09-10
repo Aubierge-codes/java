bf15a6b70d814c85235665fa50b24131c644d8a0
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-10 08:58:01 +0200

    Learn Java constructors

--- week1/Objects.java
+++ week1/Objects.java
@@ -2,15 +2,11 @@ public class Objects {
 
     public static void main(String[] args) {
 
-        Student student1 = new Student();
+        Student student1 = new Student("Aubierge", 16);
+        Student student2 = new Student("Flora", 17);
 
-        student1.name = "Aubierge";
-        student1.age = 16;
-        student1.school = "High School";
-        student1.study();
         student1.introduce();
-        System.out.println(student1.pass(75));
-        System.out.println(student1.pass(40));
+        student2.introduce();
     }
 }
 
@@ -18,20 +14,13 @@ class Student {
 
     String name;
     int age;
-    String school;
-    char grade;
-    
-    
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     void introduce() {
         System.out.println("My name is " + name + " and I am " + age + " years old.");
     }
-    void study() {
-    System.out.println(name + " is studying.");
-}
-
-    boolean pass(int score) {
-    return score >= 50;
-}
-
 }

094cd7a10f61b1f4118c058f60b37541c6cfe282
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-11 21:22:34 +0200

    Learn Java packages

---
+++ week1/packages/Student.java
@@ -0,0 +1,14 @@
+package packages;
+
+public class Student {
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public void introduce() {
+        System.out.println("My name is " + name);
+    }
+}

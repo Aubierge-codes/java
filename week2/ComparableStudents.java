92d6320ebaacede6bec53bfe03c6ce882eee42c7
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-12 20:35:54 +0200

    Learn Comparable and object sorting

--- week2/ComparableStudents.java
+++ week2/ComparableStudents.java
@@ -5,34 +5,38 @@ public class ComparableStudents {
 
     public static void main(String[] args) {
 
-        ArrayList<Student> students = new ArrayList<>();
+        ArrayList<ComparableStudent> students = new ArrayList<>();
 
-        students.add(new Student("Liam", 18));
-        students.add(new Student("Aubierge", 16));
-        students.add(new Student("Flora", 17));
+        students.add(new ComparableStudent("Liam", 18));
+        students.add(new ComparableStudent("Aubierge", 16));
+        students.add(new ComparableStudent("Flora", 17));
 
         System.out.println("Before sorting:");
 
-        for (Student student : students) {
-            System.out.println(student.getName() + " - " + student.getAge());
+        for (ComparableStudent student : students) {
+            System.out.println(
+                student.getName() + " - " + student.getAge()
+            );
         }
 
         Collections.sort(students);
 
         System.out.println("\nAfter sorting by age:");
 
-        for (Student student : students) {
-            System.out.println(student.getName() + " - " + student.getAge());
+        for (ComparableStudent student : students) {
+            System.out.println(
+                student.getName() + " - " + student.getAge()
+            );
         }
     }
 }
 
-class Student implements Comparable<Student> {
+class ComparableStudent implements Comparable<ComparableStudent> {
 
     private String name;
     private int age;
 
-    public Student(String name, int age) {
+    public ComparableStudent(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -46,7 +50,7 @@ class Student implements Comparable<Student> {
     }
 
     @Override
-    public int compareTo(Student other) {
+    public int compareTo(ComparableStudent other) {
         return Integer.compare(this.age, other.age);
     }
 }

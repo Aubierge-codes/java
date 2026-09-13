c403ebbea803761f09d8119b74828d96474a75c4
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-13 22:11:51 +0200

    Add Java generics lesson

---
+++ week1/Generics.java
@@ -0,0 +1,24 @@
+public class Generics {
+
+    public static void main(String[] args) {
+
+        Box<String> nameBox = new Box<>("Aubierge");
+        Box<Integer> ageBox = new Box<>(16);
+
+        System.out.println(nameBox.getValue());
+        System.out.println(ageBox.getValue());
+    }
+}
+
+class Box<T> {
+
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}

c079519185ecefa22638a5857abc83c30140adf0
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-13 22:15:47 +0200

    Add Course model

---
+++ week2/student-management/src/model/Course.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Course {
+
+    private int id;
+    private String name;
+    private int credits;
+
+    public Course(int id, String name, int credits) {
+        this.id = id;
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public void displayCourse() {
+        System.out.println(
+                "ID: " + id +
+                " | Course: " + name +
+                " | Credits: " + credits
+        );
+    }
+}

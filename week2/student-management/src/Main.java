017cdb46df2eeca43364f1bf66a4d038f2cfa241
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-13 21:56:11 +0200

    Create student management project foundation

--- week2/student-management/src/Main.java
+++ week2/student-management/src/Main.java
@@ -1,5 +1,30 @@
-package student-management.src;
+import model.Student;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     STUDENT MANAGEMENT SYSTEM");
+        System.out.println("=================================");
+
+        Student student1 = new Student(
+                101,
+                "Aubierge",
+                "aubierge@example.com",
+                "Software Development"
+        );
+
+        Student student2 = new Student(
+                102,
+                "Flora",
+                "flora@example.com",
+                "Embedded Systems"
+        );
+
+        System.out.println("\nRegistered Students:");
+
+        student1.display();
+        student2.display();
+    }
 }
